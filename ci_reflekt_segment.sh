c4665c62ec57dce8e3ddb30f3d2d5675e766daca
Greg Clunies <greg.clunies@gmail.com>
2022-08-16 23:54:00 -0700

    chore: comment for clarity

--- ci_reflekt_segment.sh
+++ ci_reflekt_segment.sh
@@ -4,26 +4,31 @@ echo "$(date +"%T")"
 echo "$(date +"%T") Searching tracking plans"
 plans=$(ls -- tracking-plans)
 
+# Get a list of tracking plans
 if [ -z "${plans}" ]; then
     echo "No tracking plans found in 'tracking-plans/'!"
-    exit 1
+    exit 1  # Exit if no tracking plans found
 fi
 
 echo "$(date +"%T") Found tracking plans:"
+
 for plan in ${plans}; do
     echo "$(date +"%T")     ${plan}"
 done;
 
+# Loop the tracking plans
 for plan in ${plans}; do
     echo "$(date +"%T")"
     echo "$(date +"%T") Checking tracking plan '${plan}' for changes..."
 
+    # Build out --update args for reflekt push
     update_str=""
     update_events=$(git diff origin --name-only --diff-filter=AMR -- tracking-plans/${plan}/events)
     update_traits=$(git diff origin --name-only --diff-filter=AMR -- tracking-plans/${plan}/user-traits.yml tracking-plans/${plan}/group-traits.yml)
     updates=("${update_events[@]}" "${update_traits[@]}")
 
     echo "$(date +"%T") Checking for new/updated events or traits..."
+
     for update_file in ${updates}; do
         update_name=$(basename ${update_file} .yml)
         update_str="$update_str-u ${update_name} "
@@ -36,12 +41,14 @@ for plan in ${plans}; do
         echo "$(date +"%T")     No new/updated events or traits detected"
     fi
 
+    # Build out --remove args for reflekt push
     remove_str=""
     remove_events=$(git diff origin --name-only --diff-filter=D -- tracking-plans/${plan}/events)
     remove_traits=$(git diff origin --name-only --diff-filter=D -- tracking-plans/${plan}/user-traits.yml tracking-plans/${plan}/group-traits.yml)
     removals=("${remove_events[@]}" "${remove_traits[@]}")
 
     echo "$(date +"%T") Checking for removed events or traits..."
+
     for removal_file in ${removals}; do
         removal_name=$(basename ${removal_file} .yml)
         remove_str="$remove_str-r ${removal_name} "
