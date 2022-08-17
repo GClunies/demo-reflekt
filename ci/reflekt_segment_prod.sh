b7cd41e48c2d8f2f6b80c8899b499ab7ebc08925
Greg Clunies <greg.clunies@gmail.com>
2022-08-17 15:44:11 -0700

    ci: check for new/update/removal within PR

--- ci/reflekt_segment_prod.sh
+++ ci/reflekt_segment_prod.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Set git refs based on provided arguments
+base_ref=$1
+head_ref=$2
+
 echo "$(date +"%T") QA CI Suite: Reflekt <-> Segment Protocols"
 echo "$(date +"%T")"
 echo "$(date +"%T") Searching Reflekt project for tracking plans..."
@@ -24,20 +29,22 @@ for plan in ${plans}; do
 
     # Build out --update args for reflekt push
     update_str=""
-    update_events=$(git diff main --name-only --diff-filter=AMR -- tracking-plans/${plan}/events)
-    update_traits=$(git diff main --name-only --diff-filter=AMR -- tracking-plans/${plan}/user-traits.yml tracking-plans/${plan}/group-traits.yml)
+    update_events=$(git diff ${base_ref} ${head_ref} --name-only --diff-filter=AMR -- tracking-plans/${plan}/events)
+    update_traits=$(git diff ${base_ref} ${head_ref} --name-only --diff-filter=AMR -- tracking-plans/${plan}/user-traits.yml tracking-plans/${plan}/group-traits.yml)
     updates=("${update_events[@]}" "${update_traits[@]}")
-
-    echo "$(date +"%T") Searching for new/updated events or traits..."
 
     for update_file in ${updates}; do
         update_name=$(basename ${update_file} .yml)
         update_str="$update_str-u ${update_name} "
     done;
 
+    echo "$(date +"%T") Searching for new/updated events or traits..."
+
     # Run reflekt push with --update args
     if [ "${update_str}" != "" ]; then
-        echo "$(date +"%T") Found new/updated events or traits. Running Reflekt command:\n$(date +"%T")\n$(date +"%T")     reflekt push -n ${plan} ${update_str}"
+        echo "$(date +"%T") Found new/updated events or traits. Running Reflekt command:"
+        echo "$(date +"%T")"
+        echo "$(date +"%T")     reflekt push -n ${plan} ${update_str}"
         echo "$(date +"%T")"
         reflekt push -n ${plan} ${update_str}
         echo "$(date +"%T")"
@@ -47,20 +54,22 @@ for plan in ${plans}; do
 
     # Build out --remove args for reflekt push
     remove_str=""
-    remove_events=$(git diff main --name-only --diff-filter=D -- tracking-plans/${plan}/events)
-    remove_traits=$(git diff main --name-only --diff-filter=D -- tracking-plans/${plan}/user-traits.yml tracking-plans/${plan}/group-traits.yml)
+    remove_events=$(git diff ${base_ref} ${head_ref} --name-only --diff-filter=D -- tracking-plans/${plan}/events)
+    remove_traits=$(git diff ${base_ref} ${head_ref} --name-only --diff-filter=D -- tracking-plans/${plan}/user-traits.yml tracking-plans/${plan}/group-traits.yml)
     removals=("${remove_events[@]}" "${remove_traits[@]}")
-
-    echo "$(date +"%T") Checking for removed events or traits..."
 
     for removal_file in ${removals}; do
         removal_name=$(basename ${removal_file} .yml)
         remove_str="$remove_str-r ${removal_name} "
     done;
 
+    echo "$(date +"%T") Checking for removed events or traits..."
+
     # Run reflekt push with --remove args
     if [ "${remove_str}" != "" ]; then
-        echo "$(date +"%T") Found removed events or traits.Running Reflekt command:\n$(date +"%T")\n$(date +"%T")     reflekt push -n ${plan} ${remove_str}"
+        echo "$(date +"%T") Found removed events or traits. Running Reflekt command:"
+        echo "$(date +"%T")"
+        echo "$(date +"%T")     reflekt push -n ${plan} ${remove_str}"
         echo "$(date +"%T")"
         reflekt push -n ${plan} ${remove_str}
         echo "$(date +"%T")"
